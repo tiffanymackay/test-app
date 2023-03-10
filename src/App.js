3812dfd9ca599b46558103a72452ca67907bf052
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2023-03-09 17:53:20 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'dribble-design-system';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Design systems component test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button label="Test" />
+
       </header>
     </div>
   );
